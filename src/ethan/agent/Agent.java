6dee6edce86d56113baa3249aa02d75f59cf1d05
ethanimf <45320308@qq.com>
2015-09-15 15:20:18 +0800

    +src

---
+++ src/ethan/agent/Agent.java
@@ -0,0 +1,14 @@
+package ethan.agent;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by vin on 9/15/15.
+ */
+public class Agent {
+
+    public void test(){
+        System.out.println("test!!!");
+    }
+
+}
---
+++ test/agent/Test.java
@@ -0,0 +1,19 @@
+package agent;
+
+import ethan.agent.Agent;
+import junit.framework.TestCase;
+
+/**
+ * Created by vin on 9/15/15.
+ */
+public class Test extends TestCase {
+
+    @org.junit.Test
+    public void test()
+
+    {
+        Agent agent = new Agent();
+        System.out.println("test!!!");
+    }
+
+}
